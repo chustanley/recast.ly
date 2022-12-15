a3f9ae3c996cc1f998229b202d898bdbe526fd53
Sarah <sarahkwando@gmail.com>
2022-12-14 23:10:22 -0600

    Complete creating a dynamic Video Player component

--- src/components/VideoPlayer.jsx
+++ src/components/VideoPlayer.jsx
@@ -1,14 +1,19 @@
-var VideoPlayer = () => (
-  <div className="video-player">
-    <div className="embed-responsive embed-responsive-16by9">
-      <iframe className="embed-responsive-item" src="https://www.youtube.com/embed/dQw4w9WgXcQ?autoplay=1" allowFullScreen></iframe>
+var VideoPlayer = (props) => {
+  var url = 'https://www.youtube.com/embed/' + props.args[1].id.videoId;
+
+
+  return (
+    <div className="video-player">
+      <div className="embed-responsive embed-responsive-16by9">
+        <iframe className="embed-responsive-item" src={url} allowFullScreen></iframe>
+      </div>
+      <div className="video-player-details">
+        <h3>{props.args[1].snippet.title}</h3>
+        <div>{props.args[1].snippet.description}</div>
+      </div>
     </div>
-    <div className="video-player-details">
-      <h3>Video Title</h3>
-      <div>Video Description</div>
-    </div>
-  </div>
-);
+  );
+};
 
 // PropTypes tell other developers what `props` a component expects
 // Warnings will be shown in the console when the defined rules are violated
