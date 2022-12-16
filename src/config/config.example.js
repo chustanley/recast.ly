ea16a74c010cc6e8c4c33c576be8e2a9c9fda8da
Sarah <sarahkwando@gmail.com>
2022-12-15 22:47:17 -0600

    Complete connecting App to the YouTube API

--- src/config/config.example.js
+++ src/config/config.example.js
@@ -1,5 +1,5 @@
 // Put your API key goes here!
-var API_KEY = 'YOUR_API_KEY_HERE';
-var YOUTUBE_API_KEY = 'YOUR_YOUTUBE_API_KEY_HERE';
+var API_KEY = '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';
+var YOUTUBE_API_KEY = 'AIzaSyDTFbHw-XudIcSYyqhuLl8jQcCAJIWQIxw';
 
 export { API_KEY, YOUTUBE_API_KEY };
